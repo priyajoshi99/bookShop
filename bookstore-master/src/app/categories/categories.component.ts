4bb5e52dd1be4e546bec3864866cc5601ea093d5
hemanth <53757852+hemanth000@users.noreply.github.com>
2021-05-06 22:48:31 +0530

    Update categories.component.ts

--- bookstore-master/src/app/categories/categories.component.ts
+++ bookstore-master/src/app/categories/categories.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute,Router,ParamMap } from '@angular/router';
 import { books } from '../book';
 import { CategoriesbooksService } from '../categoriesbooks.service';
+import { RegisteruserService } from '../registeruser.service';
+import { AddproductstocartService } from '../addproductstocart.service';
 
 
 @Component({
@@ -13,12 +15,30 @@ export class CategoriesComponent implements OnInit {
 
   name:any
   bookcategories:books[]=[]
-  constructor(private route:ActivatedRoute,private cs:CategoriesbooksService,private routes:Router) { 
+  loggeduser:any
+  constructor(private route:ActivatedRoute,private cs:CategoriesbooksService,private routes:Router,private rs:RegisteruserService,private ap:AddproductstocartService) { 
     
   }
   navigateto(val:any){
         console.log(val);
          this.routes.navigate([`productdetails/${val}`])
+  }
+  
+  gotocart(i:any){
+    let cart={
+      uid:this.loggeduser,
+      pid:this.bookcategories[i]._id,
+      title:this.bookcategories[i].title,
+      price:this.bookcategories[i].price,
+      qty:1,
+      discount:this.bookcategories[i].discount,
+    }
+
+   this.ap.addproducts(cart).subscribe((data)=>{
+     console.log(data);
+     
+   })
+    this.routes.navigate(['/addtocart'])
   }
 
   ngOnInit() {
@@ -27,6 +47,7 @@ export class CategoriesComponent implements OnInit {
          this.name=params.get('name')
          this.cs.getbooksbycategories(this.name).subscribe(data=>this.bookcategories=data)
         console.log(this.bookcategories)
+      this.loggeduser=this.rs.getuserid()
     })
     
   }
